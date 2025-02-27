2f59ec0d949e793a1e3a3efbc7b4cfcc1c277dc7
thirawatnalampang <thirawat.na@ku.th>
2025-02-27 20:27:53 +0700

    Initial commit

---
+++ Frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          MiniStore
+        </q-toolbar-title>
+
+        <q-tabs align="left">
+        <q-route-tab to="/" label="Home" />
+        <q-route-tab to="/about" label="About" />
+      </q-tabs>
+
+    </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {{Q}}
+const columns = [
+  { name: 'customer_id', align: 'center', label: 'customer_id', field: 'customer_id', sortable: true },
+  { name: 'first_name', align: 'center', label: 'first_name', field: 'first_name', sortable: true },
+  { name: 'last_name', align: 'center', label: 'last_name', field: 'last_name', sortable: true },
+  { name: 'email', align: 'center', label: 'email', field: 'email', sortable: true },
+  { name: 'address', align: 'center', label: 'address', field: 'address', sortable: true },
+  { name: 'phone_number', align: 'center', label: 'phone_number', field: 'phone_number', sortable: true },
+  { name: 'actions', align: 'center', label: 'id', field: 'id', sortable: true }
+]
+const rows = ([])
+const fetchData = async () => {
+  fetch('http://localhost:8800/api/v1/customers')
+    .then(res => response.json())
+    .then(result => {
+      rows.value = result
+    })
+}
+
+fetchData()
+</script>
